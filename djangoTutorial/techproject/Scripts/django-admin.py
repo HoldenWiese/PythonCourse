50466ea3bd1afe15d56bd51b5b5cc41fd01a905a
Holden Wiese <wieseholden@gmail.com>
2020-03-02 15:27:49 -0700

    Add Django project files.

---
+++ djangoTutorial/techproject/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\wiese\onedrive\documents\gitrepositories\pythoncourse\djangotutorial\techproject\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
