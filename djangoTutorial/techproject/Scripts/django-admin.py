7b8dbe6ffac924816086219fcdeaadf6cf1decba
Holden Wiese <wieseholden@gmail.com>
2020-03-05 23:17:48 -0700

    Starting a bunch a test django projects and what not.

--- djangoTutorial/techproject/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\wiese\onedrive\documents\gitrepositories\pythoncourse\djangotutorial\techproject\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
